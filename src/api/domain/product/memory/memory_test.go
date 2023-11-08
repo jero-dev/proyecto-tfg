22e3170c32e1e3962509260933925bea5b1a3ad7
Jerónimo Chaves Caballero <jerocc_dev@outlook.com>
2023-11-08 17:34:18 +0100

    #38 - Implementar el servicio de gestión de ofertas
    * #38 - Añadir servicio de gestión de ofertas
    
    * Cambiar el título de la sección
    
    * Arreglar fallos encontrados por el linter
    
    * Replantear los elementos comentados en la PR

--- src/api/domain/product/memory/memory_test.go
+++ src/api/domain/product/memory/memory_test.go
@@ -166,6 +166,72 @@ func TestMemoryProductRepository_GetByNameAndPlatform(t *testing.T) {
 	}
 }
 
+func TestMemoryProductRepository_GetByName(t *testing.T) {
+	repo := New()
+
+	type testCase struct {
+		name             string
+		products         []aggregates.Product
+		videoGameName    string
+		expectedProducts []aggregates.Product
+		expectedError    error
+	}
+
+	existingProduct, _ := aggregates.NewProduct(testVideoGameName, testVideoGamePlatform)
+	existingProductInDifferentPlatform, _ := aggregates.NewProduct(testVideoGameName, "Not Test Platform")
+	existingProductWithDifferentName, _ := aggregates.NewProduct("Not Test Product", testVideoGamePlatform)
+
+	testCases := []testCase{
+		{
+			name:             "No products in repository",
+			products:         []aggregates.Product{},
+			videoGameName:    testVideoGameName,
+			expectedProducts: []aggregates.Product{},
+			expectedError:    product.ErrorProductNotFound,
+		},
+		{
+			name:             "Products in repository but no matching name",
+			products:         []aggregates.Product{existingProduct, existingProductInDifferentPlatform, existingProductWithDifferentName},
+			videoGameName:    "Test Product Name",
+			expectedProducts: []aggregates.Product{},
+			expectedError:    product.ErrorProductNotFound,
+		},
+		{
+			name:             "Products in repository and matching name",
+			products:         []aggregates.Product{existingProduct, existingProductInDifferentPlatform, existingProductWithDifferentName},
+			videoGameName:    testVideoGameName,
+			expectedProducts: []aggregates.Product{existingProduct, existingProductInDifferentPlatform},
+			expectedError:    nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			for _, product := range testCase.products {
+				repo.Add(product)
+			}
+
+			testProducts, testError := repo.GetByName(testCase.videoGameName)
+			if testError != testCase.expectedError {
+				t.Errorf("Expected error to be %v but got %v", testCase.expectedError, testError)
+			}
+			if len(testProducts) != len(testCase.expectedProducts) {
+				t.Errorf("Expected products to be %v but got %v", testCase.expectedProducts, testProducts)
+			}
+			for index, testProduct := range testProducts {
+				if testProduct.GetName() != testCase.expectedProducts[index].GetName() {
+					t.Errorf("Expected product name to be %v but got %v",
+						testCase.expectedProducts[index].GetName(), testProduct.GetName())
+				}
+				if testProduct.GetPlatform() != testCase.expectedProducts[index].GetPlatform() {
+					t.Errorf("Expected product platform to be %v but got %v",
+						testCase.expectedProducts[index].GetPlatform(), testProduct.GetPlatform())
+				}
+			}
+		})
+	}
+}
+
 func TestMemoryProductRepository_Add(t *testing.T) {
 	repo := New()
 
--- src/api/service/offer_manager.go
+++ src/api/service/offer_manager.go
@@ -1,11 +1,135 @@
 // Package services holds all the services that connects repositories into a business flow
 package services
 
-import aggregates "vidya-sale/api/aggregate"
+import (
+	"errors"
+	aggregates "vidya-sale/api/aggregate"
+	"vidya-sale/api/domain/product"
+	"vidya-sale/api/domain/product/memory"
+	valueobjects "vidya-sale/api/valueobject"
+)
 
-// OfferManager is an interface that defines the rules around what an offer manager
-// service has to be able to perform
+var (
+	// ErrorThereAreNoOffers is returned when there are no offers retrieve.
+	ErrorThereAreNoOffers = errors.New("there are no offers for the given game")
+)
+
 type OfferManager interface {
-	StoreOffer(message string) error
-	GetProductOffers(productName string) ([]aggregates.Product, error)
+	AddOffer(gameName, gamePlatform, offerLink string, offerPrice float64) error
+	GetGameOffers(gameName string) ([]aggregates.Product, error)
+}
+
+// OfferManagerConfiguration is an alias for a function that will take in a pointer to
+// a OfferManagerService and modify it
+type OfferManagerConfiguration func(managerService *OfferManagerService) error
+
+// OfferManagerService is an implementation of the OfferManagerService
+type OfferManagerService struct {
+	products product.ProductRepository
+}
+
+// NewOfferManagerService takes a variable amount of OfferManagerConfiguration
+// functions and returns a new OfferManagerService.
+// Each OfferManagerConfiguration will be called in the order they are passed in
+func NewOfferManagerService(configurations ...OfferManagerConfiguration) (*OfferManagerService, error) {
+
+	managerService := &OfferManagerService{}
+
+	for _, configuration := range configurations {
+		err := configuration(managerService)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return managerService, nil
+}
+
+// WithProductRepository applies a given product repository to the OfferManagerService
+func WithProductRepository(productRepository product.ProductRepository) OfferManagerConfiguration {
+
+	return func(processorService *OfferManagerService) error {
+		processorService.products = productRepository
+		return nil
+	}
+}
+
+// WithMemoryProductRepository applies a memory product repository to the OfferManagerService
+func WithMemoryProductRepository() OfferManagerConfiguration {
+	productRepository := memory.New()
+	return WithProductRepository(productRepository)
+}
+
+// StoreOffer gets a message and stores the offer announced in the repository
+func (managerService *OfferManagerService) StoreOffer(gameName, gamePlatform, offerLink string, offerPrice float64) error {
+	foundProduct, findingError := managerService.products.GetByNameAndPlatform(gameName, gamePlatform)
+
+	if findingError != nil {
+		addingError := managerService.addProduct(gameName, gamePlatform, offerLink, offerPrice)
+
+		return addingError
+	} else {
+		updatingError := managerService.updateProduct(foundProduct, offerLink, offerPrice)
+
+		return updatingError
+	}
+}
+
+// GetGameOffers gets a video game name and returns all the offers for that video game with all the respective platforms
+func (managerService *OfferManagerService) GetGameOffers(gameName string) (map[string][]valueobjects.Offer, error) {
+	foundProducts, findingError := managerService.products.GetByName(gameName)
+
+	if findingError != nil {
+		return nil, ErrorThereAreNoOffers
+	}
+
+	var platformOffers = make(map[string][]valueobjects.Offer)
+
+	for _, product := range foundProducts {
+		platformOffers[product.GetPlatform()] = product.GetOffers()
+	}
+
+	return platformOffers, nil
+}
+
+// addProduct gets a name, a platform, a link and a price and adds the resulting product to the repository
+func (managerService *OfferManagerService) addProduct(name, platform, link string, price float64) error {
+	newProduct, createProductError := aggregates.NewProduct(name, platform)
+
+	if createProductError != nil {
+		return createProductError
+	}
+
+	newOffer, createOfferError := valueobjects.NewOffer(link, price)
+
+	if createOfferError != nil {
+		return createOfferError
+	}
+
+	newProduct.AddOffer(newOffer)
+	addProductError := managerService.products.Add(newProduct)
+
+	if addProductError != nil {
+		return addProductError
+	}
+
+	return nil
+}
+
+// updateProduct gets a product, a link and a price and updates it in the repository
+func (managerService *OfferManagerService) updateProduct(product aggregates.Product, link string, price float64) error {
+	newOffer, createOfferError := valueobjects.NewOffer(link, price)
+
+	if createOfferError != nil {
+		return createOfferError
+	}
+
+	product.AddOffer(newOffer)
+	updateProductError := managerService.products.Update(product)
+
+	if updateProductError != nil {
+		return updateProductError
+	}
+
+	return nil
 }
