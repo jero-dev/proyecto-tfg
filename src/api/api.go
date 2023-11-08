c2fb92f104dcbb82183fba132cea0b15012c73ef
Jerónimo Chaves <jerocc_dev@outlook.com>
2023-11-08 18:25:27 +0100

    Añadir la capa de API a la solución

--- src/api/api.go
+++ src/api/api.go
@@ -1,21 +1,91 @@
 package main
 
 import (
+	services "vidya-sale/api/service"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+	api := app.Group("/api", logger.New())
+	listenPort := ":8080"
 
-	api := app.Group("/api")
+	managerService, serviceError :=
+		services.NewOfferManagerService(services.WithMemoryProductRepository())
 
-	api.Get("/hello", func(context *fiber.Ctx) error {
-		return context.SendString("Hello, World!")
+	if serviceError != nil {
+		panic(serviceError)
+	}
+
+	processorService := services.NewMessageProcessorService()
+
+	api.Get("/offers/:gameName", func(context *fiber.Ctx) error {
+		return getGameOffers(context, managerService)
+	})
+	api.Post("/offers", func(context *fiber.Ctx) error {
+		return storeOffer(context, managerService, processorService)
 	})
 
-	apiError := app.Listen(":3000")
+	app.Listen(listenPort)
+}
 
-	if apiError != nil {
-		panic(apiError)
+func getGameOffers(context *fiber.Ctx, offerManager *services.OfferManagerService) error {
+	gameName := context.Params("gameName")
+
+	gameOffers, getGameOffersError := offerManager.GetGameOffers(gameName)
+
+	if getGameOffersError != nil {
+		return context.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"success": false,
+			"message": "Could not find offers for the related game.",
+		})
 	}
+
+	return context.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"message": "Game offers successfully retrieved.",
+		"data":    gameOffers,
+	})
+}
+
+func storeOffer(context *fiber.Ctx, offerManager *services.OfferManagerService,
+	processorService *services.MessageProcessorService) error {
+
+	messageRequest := &MessageRequest{}
+
+	if parseError := context.BodyParser(messageRequest); parseError != nil {
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Could not parse request body.",
+		})
+	}
+
+	gameName, platform, link, price := processorService.ParseMessage(messageRequest.Message)
+
+	storingError := offerManager.StoreOffer(gameName, platform, link, price)
+
+	if storingError != nil {
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Could not find the elements correctly.",
+			"data": fiber.Map{
+				"incomingMessage": messageRequest.Message,
+				"gameName":        gameName,
+				"platform":        platform,
+				"link":            link,
+				"price":           price,
+			},
+		})
+	}
+
+	return context.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"message": "Offer successfully stored.",
+	})
+}
+
+type MessageRequest struct {
+	Message string `json:"message"`
 }
